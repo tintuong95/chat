60cfe98ca4a975b85bf46baf2f926f526d103bac
tintuong <tintuong95@gmail.com>
2022-12-04 11:23:43 +0700

    home 12.04

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const http = require("http");
-const { DatabaseError } = require("sequelize");
+const { DatabaseError, Op } = require("sequelize");
 const sequelize = require("./config/sequelize.js");
 const Message = require("./db/models/message.js");
 const Rooms = require("./db/models/room.js");
@@ -18,14 +18,14 @@ const io = require("socket.io")(server, {
   },
 });
 
-sequelize.sync({alter:true})
+sequelize.sync({ alter: true })
 
 io.on("connection", (socket) => {
 
-  socket.on("join_room_client",async () => {
+  socket.on("join_room_client", async () => {
     await createRoom(socket.id);
     await socket.join(socket.id);
-    io.to(socket.id).emit("roomId",{id:socket.id})
+    io.to(socket.id).emit("roomId", { id: socket.id })
   });
 
   socket.on("list_message_client", async ({ roomId }) => {
@@ -35,35 +35,36 @@ io.on("connection", (socket) => {
     io.to(roomId).emit("receive_message_client", { data: response });
   });
 
-  socket.on("send_message_client", async ({roomId,data}) => {
-    try{
+  socket.on("send_message_client", async ({ roomId, data }) => {
+    try {
       if (!data.state) {
-       const response =await Rooms.findOne({where:{roomId}})
-       response.update({count:response.getDataValue("count")+1})
-      const filterList = Object.entries(question).filter((item) => {
-        return item[0].includes(data.content);
-      });
-      if (filterList.length > 0) {
-        await showQuestion(filterList, data);
+        const response = await Rooms.findOne({ where: { roomId } })
+        response.update({ count: response.getDataValue("count") + 1 })
+        const filterList = Object.entries(question).filter((item) => {
+          return item[0].includes(data.content);
+        });
+       
+        if (filterList.length > 0) {
+          await showQuestion(filterList, data);
+        } else {
+          await Message.bulkCreate([data]);
+        }
       } else {
-        await Message.bulkCreate([data]);
-      }
-    } else {
-     
-      await Message.create({ ...data, content: question[data.content] });
-    }
 
-    //response
-    const response = await Message.findAll({
-      where: { roomId: data.roomId },
-      order: [["createdAt", "ASC"]],
-    });
-    io.to(roomId).emit("receive_message", { data: response });
-    }catch(e){
-    
+        await Message.create({ ...data, content: question[data.content] });
+      }
+
+      //response
+      const response = await Message.findAll({
+        where: { roomId: data.roomId },
+        order: [["createdAt", "ASC"]],
+      });
+      io.to(roomId).emit("receive_message", { data: response });
+    } catch (e) {
+
       console.log(e)
     }
-    
+
   });
 
   socket.on("changeStatusRoom", async ({ roomId }) => {
@@ -75,8 +76,8 @@ io.on("connection", (socket) => {
 
   //admin
   socket.on("join_room", async ({ roomId }) => {
-     const response =await Rooms.findOne({where:{roomId}})
-       response.update({count:0})
+    const response = await Rooms.findOne({ where: { roomId } })
+    response.update({ count: 0 })
     socket.join(roomId);
   });
 
@@ -98,7 +99,11 @@ io.on("connection", (socket) => {
   });
 
   socket.on("roomList", async (data) => {
-    const response = await Rooms.findAll();
+    const response = await Rooms.findAll({
+      where: { count :{
+        [Op.gt]: 0, 
+      }}
+    });
     io.emit("roomList", { data: response });
   });
 });
